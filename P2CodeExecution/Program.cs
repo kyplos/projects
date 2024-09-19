e413d88b958d935ff901d976b3426d529c0c4f88
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-09-19 11:05:58 +0200

    secound assignment

---
+++ P2CodeExecution/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("this is line 1.");
+Console.WriteLine("this is line 2.");
+Console.WriteLine("this is line 3.");Console.WriteLine("this is line 4.");
+Console.WriteLine("this is line 5.");
+
+
+
+
+Console.WriteLine("this is line 6.");
