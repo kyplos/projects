d1aa8e70b23923b1961dfb1940f2006ae9f632bb
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-09-19 11:27:37 +0200

    fifth assignment

---
+++ P5Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string A, B;
+
+A = "Hello";
+ Console.WriteLine(A);
+ A = "World";
+ Console.WriteLine(A);
+ B = A;
+ Console.WriteLine(B);
+ A = "forsbergs";
+ Console.WriteLine(A);
+ Console.WriteLine(B);
