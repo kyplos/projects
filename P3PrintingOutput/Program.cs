23934606ed40c0089810c891d32618fc035d8d5f
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-09-19 11:11:52 +0200

    third assignment

---
+++ P3PrintingOutput/Program.cs
@@ -0,0 +1,5 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to 102 CSharp Basics!");
+Console.WriteLine("It´s easy to print outputs...");
+Console.WriteLine("...to the console!");
