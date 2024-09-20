5cec95311bc84d4b0bdf0b00485a8f602ef17f2c
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-09-20 15:00:01 +0200

    Conversion assignment

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("give me a number.");
+string num = Console.ReadLine();
+float num2 = Convert.ToSingle(num);
+Console.WriteLine(num2);
+int num3 = Convert.ToInt32(num2);
+Console.WriteLine(num3);
+int num4 = Convert.ToInt32(num);
+Console.WriteLine(num4);
