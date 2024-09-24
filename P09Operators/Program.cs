462e7c7279e2a43d6f2041c6d6e1d21cab8353a3
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-09-24 16:23:08 +0200

    ninth assignment

---
+++ P09Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("give me a number of seconds");
+string Seconds = Console.ReadLine();
+
+int Second = int.Parse(Seconds);
+
+if(Second < 0){
+    Console.WriteLine("please input a valid number");
+}
+else
+{
+    int Days = Second / (24 * 3600);
+    Console.WriteLine("Days:"+ Days);    
+     int Remaningseconds = Second % (24 * 3600);
+        
+    int hours = Remaningseconds / 3600;
+    Console.WriteLine("Hours:"+ hours);    
+    Remaningseconds %= 3600;    
+        
+    int Minutes = Remaningseconds / 60;
+    Console.WriteLine("Minutes:"+ Minutes);    
+    
+    int rSeconds = Remaningseconds % 60;
+    Console.WriteLine("Seconds:"+ rSeconds);
+    
+        
+}
+
+
+ 
+
+
+
