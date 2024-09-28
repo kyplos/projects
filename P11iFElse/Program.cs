816b3a16dabf6a1ba51c088617cd5ef3b56008b0
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-09-28 14:11:21 +0200

    11th assignment

---
+++ P11iFElse/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("what´s your age?");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age <= 13)
+{
+    Console.WriteLine("you are a child");
+}
+else if (age >= 14 && age <= 17)
+{
+    Console.WriteLine("you are a teenager");
+}
+else if (age >= 18)
+{
+    Console.WriteLine("you are a adult");
+}
+Console.WriteLine("give me another number");
