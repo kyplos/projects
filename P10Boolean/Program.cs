0234cc301e1c80acc6650bada4e0cb11af0e134a
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-09-28 09:01:43 +0200

    10th assignment

---
+++ P10Boolean/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("what is your age?");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+bool isChild = age <= 13;
+bool isTeenager = age >= 14 && age <= 17;
+bool isAdult = age >= 18;
+
+Console.WriteLine($"you are a child:{isChild}");
+Console.WriteLine($"you are a teenager:{isTeenager}");
+Console.WriteLine($"you are a adult:{isAdult}");
