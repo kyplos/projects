a3b7fe3f04c10d3c82cee0ce79d2ee7c9a50d844
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-09-29 12:53:34 +0200

    12th assignment

---
+++ P12Random/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("please pass me a seed (integer)");
+int seed = Convert.ToInt32(Console.ReadLine());
+Random specificrandomseed = new Random(seed);
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(specificrandomseed.Next(0, 6));
+}
+Console.WriteLine("three numbers between 0.0 and 0.5");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(specificrandomseed.NextDouble()* (0.5));
+}
+Console.WriteLine("three numbers between 0.0 and 0.7");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(specificrandomseed.NextDouble()*(0.7-0.2)+0.2);
+}
+Random rand  = new Random();
+Console.WriteLine("give me a crit chance between 0,0 (0%) and 1.0 (100%)");
+double chanceOfSuccess = Convert.ToDouble(Console.ReadLine());
+
+for (int i = 0; i < 10; i++)
+{ 
+    double roll = rand.NextDouble();
+
+    if (chanceOfSuccess > roll)
+    {
+        Console.WriteLine("Success");
+    }
+    else
+    {
+        Console.WriteLine("Failure");
+    }
+}
