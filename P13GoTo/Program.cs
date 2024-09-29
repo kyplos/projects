da973c63c3bafc796c0f99b2b0ab783aad2594ad
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-09-29 13:47:07 +0200

    13th assignment

---
+++ P13GoTo/Program.cs
@@ -0,0 +1,23 @@
+﻿Random random = new Random();
+int Mynumber = random.Next(1, 101);
+Console.WriteLine("i have picked a number (1-100).it´s your turn to guess it!");
+
+bool running = true;
+while (running)
+{
+    int userinput = Convert.ToInt32(Console.ReadLine());
+    
+    if (Mynumber < userinput)
+    {
+        Console.WriteLine("your turn is too high!");
+    }
+    if (Mynumber > userinput)
+    {
+        Console.WriteLine("your turn is too low!");
+    }
+    if (Mynumber == userinput)
+    {
+        Console.WriteLine("your got the number!");
+        break;
+    }
+}
